4bb07eaa4b022f828191e39ee0dac97c5d91bcef
0bby1 <cesarava06@gmail.com>
2026-03-05 11:26:56 -0600

    Cambiamos puerto

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en puerto 3000. http://localhost:3000/");
+app.listen(3003, () => {
+  console.log("Servidor corriendo en puerto 3003. http://localhost:3003/");
 });
